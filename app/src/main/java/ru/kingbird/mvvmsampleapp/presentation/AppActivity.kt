1d7b9bf540e94d4834842122a16399efe7cc5658
denisnosov <d.nosov@kingbird.ru>
2020-09-26 15:56:24 +0300

    dependencies added

--- app/src/main/java/ru/kingbird/mvvmsampleapp/presentation/AppActivity.kt
+++ app/src/main/java/ru/kingbird/mvvmsampleapp/presentation/AppActivity.kt
@@ -2,13 +2,17 @@ package ru.kingbird.mvvmsampleapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import ru.kingbird.mvvmsampleapp.R
 
+@AndroidEntryPoint
 class AppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
 }
